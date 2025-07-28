ce06372b4414b10d4985b4997442e65499dec93d
Hussnain83 <hussnaindogar2006@gmail.com>
2025-07-28 16:12:49 +0500

    Done with user Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const User = require("./../models/User");
 const { jwtAuthMiddleware, generateToken } = require("../jwt");
+
+// check for admin role
+
+const checkAdminRole = async (userID) => {
+    try{
+        const user = await User.findById(userID);
+        if(user.role == "admin"){
+            return true;
+        }
+    }catch(err){
+        return false;
+
+    }
+}
+
+
+
+
 // Signup 
 
 router.post("/signup", async(req, res)=> {
@@ -53,6 +71,69 @@ router.post("/login", async(req, res)=> {
     catch(err){
          console.log(err);
         res.status(500).json({error: "Internal server error"});
+    }
+})
+
+// Get the details of the user by givin token
+
+router.get("/me", jwtAuthMiddleware, async(req,res)=> {
+    try{
+        const userData = req.user;
+        console.log("User Data: ", userData);
+        const userId = userData.id; // get id from userData object
+        const user = await User.findById(userId);
+        res.status(200).json({user});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal Server error"});        
+    }
+})
+
+// delete to delete user from the token
+
+router.delete("/:userID", jwtAuthMiddleware, async(req, res)=> {
+    try{
+
+        const userID = req.params.userID;
+        // checks if the params and the token id is the same 
+
+        if (req.user.id !== userID) {
+            return res.status(403).json({ error: "Access denied" });
+        }
+
+        const response = await User.findByIdAndDelete(userID);
+        
+        if(!response){
+            return res.status(404).json({error: "Candidate not found"});
+        }
+
+        console.log("User deleted");
+        res.status(200).json({response, message: `${userID}: deleted`});
+    }
+    catch(err){
+         console.log(err);
+        res.status(500).json({error: "Internal Server error"});
+    }
+})
+
+
+// list of all users only for the admin
+
+router.post ("/users",jwtAuthMiddleware, async (req, res) => {
+    try{
+        const userData = req.user;
+        const userID = userData.id;
+        if(! await checkAdminRole(userID))
+            return res.status(403).json({message: "This is only for admin "});
+
+        const users = await User.find();
+        res.status(200).json({users});
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal Server error"});
     }
 })
 
