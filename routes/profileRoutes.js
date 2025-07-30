74d1c05711192becb514c16b0e10b96b4ce91ae3
Hussnain83 <hussnaindogar2006@gmail.com>
2025-07-30 11:57:52 +0500

    Done with profile Routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -111,7 +111,134 @@ router.get("/me", jwtAuthMiddleware, async(req,res)=>{
         console.log(err);
         res.status(500).json({error: "Internal Server error"});
     }
-
 })
+
+// Delete the whole profile
+
+router.delete("/", jwtAuthMiddleware, async(req, res)=> {
+    try{
+
+        const userID = req.user.id;
+       // get id from the token      
+
+        const response = await Profile.findOneAndDelete({user: userID});
+        
+        if(!response){
+            return res.status(404).json({error: "Candidate not found"});
+        }
+
+        console.log("Profile deleted");
+        res.status(200).json({ message: `${userID}: profile deleted`});
+    }
+    catch(err){
+         console.log(err);
+        res.status(500).json({error: "Internal Server error"});
+    }
+})
+
+// update the experience of the Profile
+
+router.put("/experience", jwtAuthMiddleware, async(req, res)=> {
+    try{
+    const userID = req.user.id;
+    const newExperience = req.body;
+
+    const profile = await Profile.findOne({user: userID});
+    if(!profile) {
+        return res.status(404).json({error: "Profile not found"});
+    }
+    
+    // Add new experience to the beginnin of the array
+    profile.experience.unshift(newExperience);
+
+    const updatedProfile = await profile.save();
+    res.status(200).json(updatedProfile);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+// delete specific experience using experience id
+
+router.delete("/experience/:expId", jwtAuthMiddleware, async (req, res)=>{
+    try{
+        const userID = req.user.id;
+        const experienceId = req.params.expId;
+
+        const profile = await Profile.findOneAndUpdate(
+            {user: userID},
+            {$pull: {experience: {_id: experienceId}}},
+            {new: true}
+        );
+        if(!profile){
+            return res.status(404).json({error: "Profile not found"});
+        }
+        console.log("Experience Deleted");
+        res.status(200).json({message: "Experience deleted", profile});
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+})
+
+// update the education
+
+router.put("/education", jwtAuthMiddleware, async(req, res) => {
+    try{
+        const userID = req.user.id;
+        const newEducation = req.body;
+
+        const profile = await Profile.findOne({user:userID});
+        
+        if(!profile) {
+        return res.status(404).json({error: "Profile not found"});
+    }
+
+        profile.education.unshift(newEducation);
+        const updatedProfile = await profile.save();
+        console.log("Education deleted");
+        res.status(200).json({updatedProfile});
+
+    }
+    catch{
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+})
+
+
+// delete the education
+
+
+router.delete("/education/:eduId", jwtAuthMiddleware, async (req, res)=>{
+    try{
+        const userID = req.user.id;
+        const educationId = req.params.eduId;
+
+        const profile = await Profile.findOneAndUpdate(
+            {user: userID},
+            {$pull: {education: {_id: educationId}}},
+            {new: true}
+        );
+        if(!profile){
+            return res.status(404).json({error: "Profile not found"});
+        }
+        console.log("Education Deleted");
+        res.status(200).json({message: "Education deleted", profile});
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+})
+
+
+
+
 
 module.exports = router;
